bb260c67955f1dcddc98c1940ce3c69e8925d986
vaishnavimore09 <vaishnavimore0904@gmail.com>
2024-10-20 15:17:33 +0530

    Personal Portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function scrollCardsLeft() {  
+    var wrapLeft = document.getElementById("wrapper");
+    wrapper.scrollLeft += 200;
+    console.log(wrapLeft);
+}
+
+function scrollCardsRight() {
+    var wrapLeft = document.getElementById("wrapper");
+    wrapper.scrollLeft -= 200;
+    console.log(wrapLeft);
+}
+
+
+
+
+
